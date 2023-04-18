09cb62972286138db7af6a2ad2e4e9408860f15f
rezzasakithati <rezza.sury@students.amikom.ac.id>
2023-04-18 23:31:08 +0700

    first commit

---
+++ laprak6/Class1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Encapsuletion
+{
+    internal class handphone
+    {
+        //field
+        public string MEREK;
+        public string MODEL;
+        protected string TAHUN;
+
+        //properties
+        public string merek
+        {
+            get { return MEREK; }
+            set { MEREK = value; }
+        }
+        public string model
+        {
+            get { return MODEL; }
+            set { MODEL = value; }
+        }
+        public string tahun
+        {
+            get { return TAHUN; }
+            set { TAHUN = value; }
+        }
+        //method
+        public void SetHandphone(string merek, string model)
+        {
+            this.merek = merek;
+            this.model = model;
+            Console.WriteLine("Merek Handphone : {0}", this.MEREK);
+            Console.WriteLine("Model Handphone : {0}", this.MODEL);
+
+        }
+        public void SetTahunkeluar(string tahun)
+        {
+            this.TAHUN = tahun;
+            Console.WriteLine("Tahun Keluaran : {0}", this.TAHUN);
+        }
+    }
+}
---
+++ laprak6/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Encapsuletion;
+
+namespace Encapsuletion 
+{
+    internal class program
+    {
+        static void Main(string[] args)
+        {
+            handphone hp1 = new handphone();
+            hp1.merek = "IPHONE";
+            hp1.model = "XR";
+            hp1.tahun = "2020";
+
+            Console.WriteLine($"Merek Handphone : {hp1.merek}" + "\n" + $"Model Handphone : {hp1.model}" + "\n" + $"Tahun Keluaran : {hp1.tahun}");
+            Console.WriteLine("=========================");
+
+            handphone hp2 = new handphone();
+            hp2.SetHandphone("ASUS", "ROG 6");
+            hp2.SetTahunkeluar("2023");
+            Console.Write(hp2);
+
+            Console.ReadKey();
+        }
+    }
+}
